5c4d872ba2cda036ec0063e3f58481707d7c2bd1
jmarquardt <jmarquardt@dockererp>
2019-10-23 22:35:16 +0200

    added HelperClasses and first refactoring
    Signed-off-by: jmarquardt <jmarquardt@dockererp>

--- src/main/java/de/abas/custom/owsp/spart/utils/esdk/DatabaseMetaData.java
+++ src/main/java/de/abas/custom/owsp/spart/utils/esdk/DatabaseMetaData.java
@@ -1,5 +1,27 @@
 package de.abas.custom.owsp.spart.utils.esdk;
 
+import java.util.List;
+
+import de.abas.custom.owsp.spart.utils.CodeTemplates;
+import de.abas.erp.common.type.enums.EnumDatabase;
+import de.abas.erp.db.DbContext;
+import de.abas.erp.db.schema.company.Vartab;
+
 public class DatabaseMetaData {
+
+	public String getDatabaseCommand(DbContext ctx, EnumDatabase dbNoDb) {
+		String vartabSearchWord = "V-" + getDatabaseNumberAsString(dbNoDb) + "-00";
+		return getVartab(ctx, vartabSearchWord).getDBCmd();
+	}
+
+	private Vartab getVartab(DbContext ctx, String vartabSearchWord) {
+		// TODO: abfangen wenn null
+		List<Vartab> selectVartab = new CodeTemplates().getSelectList(Vartab.class, "such", vartabSearchWord, ctx);
+		return selectVartab.iterator().next();
+	}
+
+	public String getDatabaseNumberAsString(EnumDatabase dbNoDb) {
+		return dbNoDb.toString().replace("(", "").replace(")", "");
+	}
 
 }
---
+++ src/main/java/de/abas/custom/owsp/spart/utils/esdk/DevelopmentFilesTODO.java
@@ -0,0 +1,46 @@
+package de.abas.custom.owsp.spart.utils.esdk;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+
+public class DevelopmentFilesTODO {
+
+	public void replaceContentInFile(File file, String oldValue, String newValue, String encoding){
+		try {
+			String currentContent = readCurrentContentFrom(file, encoding);
+			String newContent = currentContent.replaceAll(oldValue, newValue);
+			
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
+			writer.write(newContent);
+			writer.close();
+			
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("Could not find file " + file.getAbsolutePath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String readCurrentContentFrom(File file, String encoding)
+			throws UnsupportedEncodingException, FileNotFoundException, IOException {
+		String currentContent = "";
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+		
+		String line = reader.readLine();
+		while (line != null) {
+			currentContent = currentContent + line + System.lineSeparator();
+			line = reader.readLine();
+		}
+		
+		reader.close();
+		return currentContent;
+	}	
+}
